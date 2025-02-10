c4345eec062e9c1eb1ad9f4792083946c7f805f0
rafopm <rafopm@gmail.com>
2025-02-09 20:29:16 -0500

    update github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://rafopm.github.io',
+    base: 'loopstudios-landing-page',
+});
